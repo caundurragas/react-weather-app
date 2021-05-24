a6cc70e56da69fbdb3e184bbfee5c73fdff1ccc8
caundurragas <caundurragas@gmail.com>
2021-05-24 17:31:57 +0100

    testing hola hola

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import "./App.css";
 import Weather from "./Weather";
 import WeeklyForecast from "./WeeklyForecast";
 import SearchEngineer from "./SearchEngineer";
-import "./index.css";
-import "./SearchForm.css";
-import "./Weather.css";
+import "./SearchEngineer.css";
 import "./WeeklyForecast.css";
+import "./Weather.css";
 
 function App() {
   return (
     <div class="container">
       <div class="weatherApp">
+        <p> Hola hola</p>
         <SearchEngineer />
         <Weather />
         <WeeklyForecast />
---
+++ src/WeeklyForecast.js
@@ -0,0 +1,61 @@
+export default function WeeklyForecast() {
+  return (
+    <div className="forecastContainer">
+      <h3>Daily</h3>
+      <div className="weatherForecast" id="daily-forecast">
+        <div className="row">
+          <div className="col-2">
+            <div className="day">Monday</div>
+            <img
+              src="https://cdn.icon-icons.com/icons2/1621/PNG/512/3741356-sun-sunny-weather_108871.png"
+              alt="weather icon"
+              className="weatherForecastIcon"
+            />
+            <div className="weatherForecastMaxTemperature">Max 24ºC</div>
+            <div className="weatherForecastMinTemperature">Min 14ºC</div>
+          </div>
+          <div className="col-2">
+            <div className="day">Tuesday</div>
+            <img
+              src="https://cdn.icon-icons.com/icons2/1621/PNG/512/3741356-sun-sunny-weather_108871.png"
+              alt="weather icon"
+              className="weatherForecastIcon"
+            />
+            <div className="weatherForecastMaxTemperature">Max 24ºC</div>
+            <div className="weatherForecastMinTemperature">Min 14ºC</div>
+          </div>
+          <div className="col-2">
+            <div className="day">Wednesday</div>
+            <img
+              src="https://cdn.icon-icons.com/icons2/1621/PNG/512/3741356-sun-sunny-weather_108871.png"
+              alt="weather icon"
+              className="weatherForecastIcon"
+            />
+            <div className="weatherForecastMaxTemperature">Max 24ºC</div>
+            <div className="weatherForecastMinTemperature">Min 14ºC</div>
+          </div>
+          <div className="col-2">
+            <div className="day">Thursday</div>
+            <img
+              src="https://cdn.icon-icons.com/icons2/1621/PNG/512/3741356-sun-sunny-weather_108871.png"
+              alt="weather icon"
+              className="weatherForecastIcon"
+            />
+            <div className="weatherForecastMaxTemperature">Max 24ºC</div>
+            <div className="weatherForecastMinTemperature">Min 14ºC</div>
+          </div>
+          <div className="col-2">
+            <div className="day">Friday</div>
+            <img
+              src="https://cdn.icon-icons.com/icons2/1621/PNG/512/3741356-sun-sunny-weather_108871.png"
+              alt="weather icon"
+              className="weatherForecastIcon"
+            />
+            <div className="weatherForecastMaxTemperature">Max 24ºC</div>
+            <div className="weatherForecastMinTemperature">Min 14ºC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
